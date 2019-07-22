3877842b26a3c689fe2c3104789464ff37f55b51
iland <ilan.dayan@checkmarx.com>
2019-07-22 16:11:52 +0300

    bugid: first commit CR_by: n/a

---
+++ src/main/java/com/cx/tool/util/FileUtil.java
@@ -0,0 +1,99 @@
+package com.cx.tool.util;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ * Created by: iland
+ * Date: 7/22/2019
+ */
+public class FileUtil {
+
+
+    public static void deleteDirectory(File directory) throws IOException {
+        if (directory.exists()) {
+            if (!isSymlink(directory)) {
+                cleanDirectory(directory);
+            }
+
+            if (!directory.delete()) {
+                String message = "Unable to delete directory " + directory + ".";
+                throw new IOException(message);
+            }
+        }
+    }
+
+    private static boolean isWindows() {
+        return System.getProperty("os.name").toLowerCase().contains("win");
+    }
+
+    private static boolean isSymlink(File file) throws IOException {
+        if (file == null) {
+            throw new NullPointerException("File must not be null");
+        } else if (isWindows()) {
+            return false;
+        } else {
+            File fileInCanonicalDir = null;
+            if (file.getParent() == null) {
+                fileInCanonicalDir = file;
+            } else {
+                File canonicalDir = file.getParentFile().getCanonicalFile();
+                fileInCanonicalDir = new File(canonicalDir, file.getName());
+            }
+
+            return !fileInCanonicalDir.getCanonicalFile().equals(fileInCanonicalDir.getAbsoluteFile());
+        }
+    }
+
+    private static void cleanDirectory(File directory) throws IOException {
+        String message;
+        if (!directory.exists()) {
+            message = directory + " does not exist";
+            throw new IllegalArgumentException(message);
+        } else if (!directory.isDirectory()) {
+            message = directory + " is not a directory";
+            throw new IllegalArgumentException(message);
+        } else {
+            File[] files = directory.listFiles();
+            if (files == null) {
+                throw new IOException("Failed to list contents of " + directory);
+            } else {
+                IOException exception = null;
+                File[] arr$ = files;
+                int len$ = files.length;
+
+                for (int i$ = 0; i$ < len$; ++i$) {
+                    File file = arr$[i$];
+
+                    try {
+                        forceDelete(file);
+                    } catch (IOException var8) {
+                        exception = var8;
+                    }
+                }
+
+                if (null != exception) {
+                    throw exception;
+                }
+            }
+        }
+    }
+
+    private static void forceDelete(File file) throws IOException {
+        if (file.isDirectory()) {
+            deleteDirectory(file);
+        } else {
+            boolean filePresent = file.exists();
+            if (!file.delete()) {
+                if (!filePresent) {
+                    throw new FileNotFoundException("File does not exist: " + file);
+                }
+
+                String message = "Unable to delete file: " + file;
+                throw new IOException(message);
+            }
+        }
+    }
+
+}
