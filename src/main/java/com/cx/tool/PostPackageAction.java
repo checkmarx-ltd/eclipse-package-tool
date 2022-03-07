93b18775cb18de9ad91488d121bf8c37cb1f8228
Subhadra.Sahoo@checkmarx.com <subhadra_sahoo@persistent.com>
2022-03-07 18:49:10 +0530

    Updated the steps to include signing eclipse plugin step and zipPlugin API

--- src/main/java/com/cx/tool/PostPackageAction.java
+++ src/main/java/com/cx/tool/PostPackageAction.java
@@ -1,9 +1,32 @@
 package com.cx.tool;
 
-import com.cx.tool.util.FileUtil;
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Objects;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.jar.JarOutputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -15,19 +38,10 @@ import javax.xml.transform.stream.StreamResult;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathFactory;
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Enumeration;
-import java.util.Objects;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
-import java.util.jar.JarOutputStream;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
-import java.util.zip.ZipInputStream;
-import java.util.zip.ZipOutputStream;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 /**
  * Created by: iland
@@ -36,40 +50,101 @@ import java.util.zip.ZipOutputStream;
 public class PostPackageAction {
 
     private static final int BUFFER_SIZE = 4096;
+    static List<File> fileList = new ArrayList<File>();  
 
     public static void main(String[] args) throws Exception {
-        FilenameFilter filter = new FilenameFilter() {
-            public boolean accept(File dir, String name) {
-                return name.startsWith("CxEclipsePlugin");
-            }
-        };
-        File srcFile = Objects.requireNonNull(new File(args[0]).listFiles(filter))[0];
+        try {
+			FilenameFilter filter = new FilenameFilter() {
+			    public boolean accept(File dir, String name) {
+			        return name.startsWith("CxEclipsePlugin");
+			    }
+			};
+			File srcFile = Objects.requireNonNull(new File(args[0]).listFiles(filter))[0];
+			File resourceLocation = new File(args[1]);
 
-        File tempDir = new File(args[0] + "\\CxEclipsePlugin-Temp");
-        String srcArtJar = args[0] + "\\CxEclipsePlugin-Temp\\artifacts.jar";
-        String trgArtJar = args[0] + "\\CxEclipsePlugin-Temp\\artifacts-temp.jar";
+			File tempDir = new File(args[0] + "\\CxEclipsePlugin-Temp");
+			String srcArtJar = args[0] + "\\CxEclipsePlugin-Temp\\artifacts.jar";
+			String trgArtJar = args[0] + "\\CxEclipsePlugin-Temp\\artifacts-temp.jar";
 
-        extract(srcFile, tempDir);
-        genFixedArtifactJar(srcArtJar, trgArtJar);
+			extract(srcFile, tempDir);
+			genFixedArtifactJar(srcArtJar, trgArtJar);
 
-        Files.deleteIfExists(Paths.get(srcArtJar));
-        new File(trgArtJar).renameTo(new File(srcArtJar));
+			Files.deleteIfExists(Paths.get(srcArtJar));
+			new File(trgArtJar).renameTo(new File(srcArtJar));
 
-        String pluginName = srcFile.getName();
+			String pluginName = srcFile.getName();
 
-        Path srcFilePath = Paths.get(srcFile.getCanonicalPath()
-                .substring(0, srcFile.getCanonicalPath().lastIndexOf(".zip")) + "-org.zip");
-        srcFile.renameTo(srcFilePath.toFile());
+			Path srcFilePath = Paths.get(srcFile.getCanonicalPath()
+			        .substring(0, srcFile.getCanonicalPath().lastIndexOf(".zip")) + "-org.zip");
+			srcFile.renameTo(srcFilePath.toFile());
 
-        String trgZip = args[0] + "\\" + pluginName;
-
-        zipPlugin(srcFilePath.toString(), trgZip, srcArtJar);
-
-        FileUtil.deleteDirectory(tempDir);
-        Files.deleteIfExists(srcFilePath);
+			String trgZip = args[0] + "\\" + pluginName;
+			copyFile(resourceLocation.getPath() ,tempDir.getPath());
+			signPlugin(tempDir);
+			deleteBatchKeystoreFiles(tempDir);
+			zipPlugin(tempDir, trgZip, srcArtJar);
+			//TODO : Will uncomment it once all testings are done satisfactorily
+//			FileUtil.deleteDirectory(tempDir);
+			Files.deleteIfExists(srcFilePath);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			System.out.println(" IN FINALLY ....");
+		}
     }
 
-    private static void zipPlugin(String orgZip, String trgZip, String trgArtJar) throws Exception {
+	private static void deleteBatchKeystoreFiles(File tempDir) {
+		 try {
+	            Files.deleteIfExists(
+	                Paths.get(tempDir+"\\" + "jar_signer.bat"));
+	            Files.deleteIfExists(
+		                Paths.get(tempDir+"\\" + "mymcre.pfx"));
+	        }
+	        catch (NoSuchFileException e) {
+	            System.out.println(
+	                "No such file/directory exists");
+	        } catch (IOException e) {
+				e.printStackTrace();
+			}		
+	}
+
+	public static void copyFile(String from, String to) throws IOException {
+		Path srcBatch = Paths.get(from+ "\\" +"jar_signer.bat");
+		Path destBatch = Paths.get(to+ "\\" +"jar_signer.bat");
+		Path srcPfx = Paths.get(from+ "\\" +"mymcre.pfx");
+		Path destPfx = Paths.get(to+ "\\" +"mymcre.pfx");
+		
+		Files.copy(srcBatch, destBatch, StandardCopyOption.COPY_ATTRIBUTES);
+		Files.copy(srcPfx, destPfx, StandardCopyOption.COPY_ATTRIBUTES);
+	}
+
+
+    private static void signPlugin(File tempDir) {
+    	ProcessBuilder processBuilder = new ProcessBuilder("cmd", "/c", "jar_signer.bat");
+    	processBuilder.directory(tempDir);
+    	try {
+            Process process = processBuilder.start();
+            StringBuilder output = new StringBuilder();
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line + "\n");
+            }
+            int exitVal = process.waitFor();
+            if (exitVal == 0) {
+                System.out.println(output);
+            } else {
+            	System.out.println("error");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private static void zipPlugin(File /*orgZip*/tempDir, String trgZip, String trgArtJar) throws Exception {/*
         ZipFile zipFile = new ZipFile(orgZip);
         final ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(trgZip));
         for (Enumeration e = zipFile.entries(); e.hasMoreElements(); ) {
@@ -98,8 +173,90 @@ public class PostPackageAction {
         }
         zos.close();
         zipFile.close();
+    */
+    	 // get list of files  
+    	
+        List<File> fileList = getFileList(tempDir);  
+        //go through the list of files and zip them   
+       zipFiles(fileList, trgZip, tempDir); 
     }
 
+    private static void zipFiles(List<File> fileList, String trgZip, File tempDir)  
+    {  
+      try  
+      {  
+        // Creating ZipOutputStream - Using input name to create output name  
+        FileOutputStream fos = new FileOutputStream(trgZip);  
+        ZipOutputStream zos = new ZipOutputStream(fos);  
+        // looping through all the files  
+        for(File file : fileList)  
+        {  
+          // To handle empty directory  
+          if(file.isDirectory())  
+          {  
+            // ZipEntry --- Here file name can be created using the source file  
+            ZipEntry ze = new ZipEntry(getFileName(file.toString(), tempDir)+"/");  
+            // Putting zipentry in zipoutputstream  
+            zos.putNextEntry(ze);  
+            zos.closeEntry();  
+          }  
+          else  
+          {  
+            FileInputStream fis = new FileInputStream(file);  
+            BufferedInputStream bis = new BufferedInputStream(fis, BUFFER_SIZE);  
+            // ZipEntry --- Here file name can be created using the source file  
+            ZipEntry ze = new ZipEntry(getFileName(file.toString(), tempDir));  
+            // Putting zipentry in zipoutputstream  
+            zos.putNextEntry(ze);  
+            byte data[] = new byte[BUFFER_SIZE];  
+            int count;  
+            while((count = bis.read(data, 0, BUFFER_SIZE)) != -1)   
+            {  
+                zos.write(data, 0, count);  
+            }  
+            bis.close();  
+            zos.closeEntry();  
+          }                 
+        }                  
+        zos.close();      
+      }  
+      catch(IOException ioExp)  
+      {  
+        System.out.println("Error while zipping " + ioExp.getMessage());  
+        ioExp.printStackTrace();  
+      }  
+    }  
+        
+   //the method returns a list of files  
+    private static List<File> getFileList(File source)  
+    {   
+      if(source.isFile())  
+      {  
+        fileList.add(source);  
+      }  
+      else if(source.isDirectory())  
+      {  
+        String[] subList = source.list();  
+        // this condition checks for empty directory  
+        if(subList.length == 0)  
+        {  
+          //System.out.println("path -- " + source.getAbsolutePath());  
+          fileList.add(new File(source.getAbsolutePath()));  
+        }  
+        for(String child : subList)  
+        {  
+          getFileList(new File(source, child));  
+        }  
+      }  
+      return fileList;  
+    }  
+        
+    private static String getFileName(String filePath, File tempDir)  
+    {  
+      String name = filePath.substring(tempDir.getAbsolutePath().length() + 1, filePath.length());  
+      return name;        
+    }  
+    
     private static void genFixedArtifactJar(String srcArtJar, String trgArtJar) throws Exception {
         JarFile artJar = new JarFile(srcArtJar);
         FileOutputStream fos = new FileOutputStream(trgArtJar);
